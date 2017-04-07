e93955f16c648c52fe29c673f38c9ec11b57c491
williamboren <willy.boren@hotmail.com>
2017-04-07 15:21:17 +0200

    Create test class to work with sending data, add Newtonsoft.Json package

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace P2P
 {
@@ -23,10 +24,71 @@ namespace P2P
         static void CreateClient(string ip, int port)
         {
             Client client = new Client(ip, port);
-            Console.WriteLine("Enter a message to send:");
-            string msg = Console.ReadLine();
-            if (client.SendData(msg)) Console.WriteLine("Success");
+
+            List<string> i = new List<string>();
+            i.Add("sword");
+            i.Add("potion");
+            Player player = new Player(100, "atk", i, 50, 50, 0.5f, 0.2f);
+
+            string data = JsonConvert.SerializeObject(player);
+            
+            if (client.SendData(data)) Console.WriteLine("Success");
             else Console.WriteLine("Failed to send data");
         }
+    }
+
+    class Player
+    {
+        private int health_points;
+        private string pClass;
+        private List<string> inventory;
+        private float posx, posy;
+        private float speedx, speedy;
+        private bool isAlive;
+
+        public Player(int hp, string c, List<string> items, float x, float y, float sx, float sy)
+        {
+            this.health_points = hp;
+            this.pClass = c;
+            this.posx = x;
+            this.posy = y;
+            this.speedx = sx;
+            this.speedy = sy;
+            this.inventory = new List<string>(items);
+            this.isAlive = true;
+        }
+
+        public void AddItem(string item)
+        {
+            inventory.Add(item);
+        }
+
+        public void RemoveItem(string item)
+        {
+            inventory.Remove(item);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            int new_hp = this.health_points - damage;
+
+            if (new_hp <= 0) this.isAlive = false;
+            else this.health_points = new_hp;
+        }
+
+        public void UpdatePos()
+        {
+            posx += speedx;
+            posy += speedy;
+        }
+
+        public int HealthPoints { get { return this.health_points; } set { this.health_points = value; } }
+        public string Class { get { return this.pClass; } set { this.pClass = value; } }
+        public List<string> Inventory { get { return this.inventory; } set { this.inventory = value; } }
+        public bool IsAlive { get { return this.isAlive; } set { this.isAlive = value; } }
+        public float SpeedX { get { return this.speedx; } set { this.speedx = value; } }
+        public float SpeedY { get { return this.speedy; } set { this.speedy = value; } }
+        public float PositionX { get { return this.posx; } set { this.posx = value; } }
+        public float PositionY { get { return this.posy; } set { this.posy = value; } }
     }
 }
